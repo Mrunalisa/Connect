3f49f2f5bb8d44916297166941692c7e16552740
notverifiedcode <101034581+Mrunalisa@users.noreply.github.com>
2023-07-24 23:15:24 +0530

    Create TodoApp

---
+++ app.js
@@ -0,0 +1,118 @@
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+document.addEventListener("DOMContentLoaded", function() {
+  const submitButton = document.getElementById("add");
+
+  submitButton.addEventListener("click", function() {
+      const inputField = document.getElementById("taskInput");
+      const item = inputField.value.trim();
+
+      if (item === "") {
+          alert("Input field is empty! Please enter something.");
+      }
+      else{
+        createItem(item)
+      }
+  });
+});
+
+document.querySelector("#taskInput").addEventListener("keypress", (e) => {
+  if(e.key === "Enter"){
+    const item = document.querySelector("#taskInput")
+    
+  }
+})
+
+function displayDate(){
+  let date = new Date()
+  date = date.toString().split(" ")
+  date =date[0]+" "+date[1] + " " + date[2] + " " + date[3] 
+  document.querySelector("#date").innerHTML = date 
+}
+
+function displayItems(){
+  let items = ""
+  for(let i = 0; i < itemsArray.length; i++){
+    items += `<div class="item">
+                <div class="input-controller">
+                  <textarea disabled>${itemsArray[i]}</textarea>
+                  <div class="edit-controller">
+                  <button class="deleteBtn">Delete</button>
+                  <button class="editBtn">Edit</button>
+                  </div>
+                </div>
+                <div class="update-controller">
+                  <button class="saveBtn">Save</button>
+                  <button class="cancelBtn">Cancel</button>
+                </div>
+              </div>`
+  }
+  document.querySelector(".todo-list").innerHTML = items
+  activateDeleteListeners()
+  activateEditListeners()
+  activateSaveListeners()
+  activateCancelListeners()
+}
+
+function activateDeleteListeners(){
+  let deleteBtn = document.querySelectorAll(".deleteBtn")
+  deleteBtn.forEach((dB, i) => {
+    dB.addEventListener("click", () => { deleteItem(i) })
+  })
+}
+
+function activateEditListeners(){
+  const editBtn = document.querySelectorAll(".editBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  editBtn.forEach((eB, i) => {
+    eB.addEventListener("click", () => { 
+      updateController[i].style.display = "block"
+      inputs[i].disabled = false })
+  })
+}
+
+function activateSaveListeners(){
+  const saveBtn = document.querySelectorAll(".saveBtn")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener("click", () => {
+      updateItem(inputs[i].value, i)
+    })
+  })
+}
+
+function activateCancelListeners(){
+  const cancelBtn = document.querySelectorAll(".cancelBtn")
+  const updateController = document.querySelectorAll(".update-controller")
+  const inputs = document.querySelectorAll(".input-controller textarea")
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener("click", () => {
+      updateController[i].style.display = "none"
+      inputs[i].disabled = true
+      inputs[i].style.border = "none"
+    })
+  })
+}
+
+function createItem(item){
+  itemsArray.push(taskInput.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+function deleteItem(i){
+  itemsArray.splice(i,1)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+function updateItem(text, i){
+  itemsArray[i] = text
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+window.onload = function() {
+  displayDate()
+  displayItems()
+};
