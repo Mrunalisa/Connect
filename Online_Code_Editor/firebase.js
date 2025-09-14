78be3af6942fbd31933c990d5b27dc385e9cb42d
Mrunali Badgujar <101034581+Mrunalisa@users.noreply.github.com>
2025-09-14 21:01:26 +0530

    Remove firebase configuration for security reasons
    Removed sensitive firebase configuration details to enhance security.

--- Online_Code_Editor/firebase.js
+++ Online_Code_Editor/firebase.js
@@ -1,13 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCYtHi7dYEWWy1cwJpUcxnd3qfMXgrpjDo",
-  authDomain: "connect-db45d.firebaseapp.com",
-  databaseURL: "https://connect-db45d-default-rtdb.firebaseio.com",
-  projectId: "connect-db45d",
-  storageBucket: "connect-db45d.appspot.com",
-  messagingSenderId: "32310119462",
-  appId: "1:32310119462:web:b6f858322706ac13f72ae7",
-  measurementId: "G-GGED3RYDCC"
-};
-
-
-firebase.initializeApp(firebaseConfig);
+Remove the firebaseConfiguration from this file for security reasons
